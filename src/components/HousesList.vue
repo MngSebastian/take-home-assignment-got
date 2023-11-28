0c8b5f25053855f21a72efce2c6b6102ea6cc963
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2023-11-28 18:16:33 +0100

    fixed houses overflow

--- src/components/HousesList.vue
+++ src/components/HousesList.vue
@@ -1,24 +1,22 @@
 <template>
-  <div
-    v-for="house in houseData"
-    :key="house.name"
-    class="flex flex-col h-full items-start w-8/12"
-  >
-    <button @click="toggleSelectedHouse(house)" class="focus:outline-none">
-      <p class="bg-blue-500 rounded-xl text-xl text-white m-2 p-2">
-        {{ house.name }}
-      </p>
-    </button>
-    <div
-      v-if="isSelectedHouse(house)"
-      class="flex flex-col items-start bg-blue-400 ml-14 mb-4 text-white text-lg rounded-xl w-4/12 p-2"
-    >
-      <h3>Members of {{ house.name }}:</h3>
-      <ul>
-        <li v-for="member in house.members" :key="member.slug">
-          {{ member.name }}
-        </li>
-      </ul>
+  <div class="flex flex-col items-start h-full w-8/12">
+    <div v-for="house in houseData" :key="house.name" class="">
+      <button @click="toggleSelectedHouse(house)" class="focus:outline-none">
+        <p class="bg-blue-500 rounded-xl text-xl text-white m-2 p-2">
+          {{ house.name }}
+        </p>
+      </button>
+      <div
+        v-if="isSelectedHouse(house)"
+        class="flex flex-col items-start bg-blue-400 ml-14 mb-4 text-white text-lg rounded-xl w-4/12 p-2"
+      >
+        <h3>Members of {{ house.name }}:</h3>
+        <ul>
+          <li v-for="member in house.members" :key="member.slug">
+            {{ member.name }}
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
